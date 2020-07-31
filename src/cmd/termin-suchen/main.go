7d2922b101ec15bbab15814a243f3aadda25da88
DH <adlandh@gmail.com>
2020-07-31 09:35:03 +0200

    refactor

--- src/cmd/termin-suchen/main.go
+++ src/cmd/termin-suchen/main.go
@@ -17,18 +17,23 @@ func main() {
 	ticker := time.NewTicker(time.Duration(configProvider.GetCheckPeriod()) * time.Second)
 	errCh := make(chan error, 1)
 	doneCh := make(chan struct{}, 1)
+	runCheck(configProvider, errCh, doneCh)
 
 	for {
 		select {
 		case <-ticker.C:
-			crawlerProvider := crawler.NewCollyCrawler(configProvider.GetVerbose(), errCh)
-
-			mainApp := app.NewApp(configProvider, crawlerProvider, errCh, doneCh)
-			mainApp.Run()
+			runCheck(configProvider, errCh, doneCh)
 		case <-doneCh:
 			os.Exit(0)
 		case err := <-errCh:
 			log.Fatal(err)
 		}
 	}
+}
+
+func runCheck(configProvider *config.Envloader, errCh chan error, doneCh chan struct{}) {
+	crawlerProvider := crawler.NewCollyCrawler(configProvider.GetVerbose(), errCh)
+
+	mainApp := app.NewApp(configProvider, crawlerProvider, errCh, doneCh)
+	mainApp.Run()
 }
